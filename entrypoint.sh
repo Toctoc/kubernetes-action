f8052e41f140a1198d4cd8f861a6247fef728c9e
Gerardo Manuel Muñoz Illesca <5135378+GMIllesca@users.noreply.github.com>
2023-07-18 16:19:59 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ echo "${KUBE_CONFIG_DATA}" | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
 echo "Arguments: $@"
-first_arg=$(echo "$1" | awk '{print $1}')
-second_arg=$(echo "$2" | awk '{print $1}')
+args_array=("$@")
+first_arg="${args_array[0]}"
 
-result="$(kubectl "$1" | sed -e ':a' -e '$!{' -e 'N' -e 'ba' -e '}' -e 's/\n/ /g')"
+result="$(kubectl "$first_arg" | sed -e ':a' -e '$!{' -e 'N' -e 'ba' -e '}' -e 's/\n/ /g')"
 status=$?
 
 echo "::set-output name=result::$result"
