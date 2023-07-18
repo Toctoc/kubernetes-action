4d78d477430ee78e2cc5fc7b35e95c0602a4d603
Gerardo Manuel Muñoz Illesca <5135378+GMIllesca@users.noreply.github.com>
2023-07-18 16:44:35 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export KUBECONFIG=kubeconfig
 echo "Arguments: $1"
 first_arg="$1"
 
-result=$(kubectl "$first_arg" | awk '{ printf "%s", $0 }')
+result=$(kubectl --record deployment.apps/financiamiento-backend set image deployment.apps/financiamiento-backend financiamiento-backend=573624488707.dkr.ecr.us-east-1.amazonaws.com/financiamiento-backend:50acebae090edfc4c4030d9bcc12a6c65af589a2)
 status=$?
 
 echo "::set-output name=result::$result"
